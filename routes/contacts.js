7a1e8d71d8cd768a52dbee6cbbb78f668dc063d5
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-12 22:48:13 +0300

    commit

---
+++ routes/contacts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contacts');
+
+router.get('/', contactsController.getAll);
+router.get('/:id', contactsController.getSingle);
+router.post('/', contactsController.createContact);
+router.put('/:id', contactsController.updateContact);
+router.delete('/:id', contactsController.deleteContact);
+
+module.exports = router;
