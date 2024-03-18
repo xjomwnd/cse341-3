6b46422c096dcd29636acf5385f6fc12df520df0
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-18 23:42:58 +0300

    commit

--- utils/errorHandlings.js
+++ utils/errorHandlings.js
@@ -1,14 +1,19 @@
+// utils/errorHandling.js
 class ValidationError extends Error {
-    constructor(message) {
-      super(message);
-      this.name = 'ValidationError';
-    }
+  constructor(errors) {
+    super('Validation Error');
+    this.errors = errors;
   }
-  
-  exports.handleErrors = (err, req, res, next) => {
-    if (err instanceof ValidationError) {
-      return res.status(400).json({ error: err.message });
-    }
-    // Handle other types of errors
-    res.status(500).json({ error: 'Internal Server Error' });
-  };
+}
+
+const handleErrors = (err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    return res.status(400).json({ errors: err.errors });
+  }
+
+  // Handle other types of errors
+  console.error(err);
+  res.status(500).json({ error: 'Internal Server Error' });
+};
+
+module.exports = { handleErrors, ValidationError };
