a325902885e84d73d4d6c804d9488317ac1e7db2
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-19 18:41:56 +0300

    commit

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/User'); // Example path to your User model
+const User = require('./routes/User'); // Example path to your User model
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
