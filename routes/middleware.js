2baa59ad01a923b0c40aa8286bd32365d3e1ab00
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-19 21:24:43 +0300

    commit

--- routes/middleware.js
+++
@@ -1,9 +0,0 @@
-// Import the authentication middleware
-const authenticateToken = require('./middleware/authentication');
-
-// Example protected route
-app.get('/protected-route', authenticateToken, (req, res) => {
-  // Access the authenticated user from req.user
-  const user = req.user;
-  res.json({ message: 'Authenticated route', user: user });
-});
