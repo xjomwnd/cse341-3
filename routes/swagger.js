c6c4e5dd36a3e1ad4b9a2ac161d946737135c161
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-20 19:22:43 +0300

    commit

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('../swaggerConfig');
+const swaggerSpec = require('./swaggerConfig');
 
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerSpec));
