d92f158ed2f7dbeea73c940efe09e6c7a1346cc5
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-20 18:16:24 +0300

    commit

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -8,6 +8,20 @@ const options = {
       version: '1.0.0',
       description: 'API documentation for your application',
     },
+    securityDefinitions: {
+      // Define security scheme for OAuth2
+      OAuth2: {
+        type: 'oauth2',
+        authorizationUrl: 'https://your-auth-provider.com/oauth2/authorize',
+        flow: 'implicit',
+        scopes: {
+          // Define scopes if required
+          read: 'Grants read access',
+          write: 'Grants write access',
+        },
+      },
+    },
+    security: [{ OAuth2: [] }], // Mark all paths as secured by default
   },
   apis: ['./routes/*.js'], // Path to the API routes files
 };
