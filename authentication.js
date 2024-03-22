244b06db11bd224185d24f290ed5b3cda8f6cfae
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-22 17:42:13 +0300

    commit

--- authentication.js
+++ authentication.js
@@ -1,6 +1,6 @@
 // Enable authentication
-use admin
-db.runCommand({setParameter: 1, authenticationMechanisms: { plain: 'mongodb_cr', 'SCRAM-SHA-1': 'scram-sha-1' }})
+use admin;
+db.runCommand({setParameter: 1, authenticationMechanisms: { plain: 'mongodb_cr', 'SCRAM-SHA-1': 'scram-sha-1' }});
 
 // Create an administrative user
 db.createUser({
