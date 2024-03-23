c8b477c132e9b0f2b8fe38bead6151ade8acd307
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-23 11:40:40 +0300

    commit

--- routes/app.js
+++ routes/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/appDBex'
+
+
 const app = express();
 
 // Import the contacts controller
